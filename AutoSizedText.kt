0a6e65e8c581bfed9d371f1a8a0ab09943a11ea6
M. Utku Ensert <97624869+mutkuensert@users.noreply.github.com>
2023-03-07 18:26:20 +0300

    Update AutoSizedText.kt

--- AutoSizedText.kt
+++ AutoSizedText.kt
@@ -1,12 +1,12 @@
 import androidx.compose.foundation.text.InlineTextContent
-import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.Text
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextStyle
@@ -22,20 +22,19 @@ import androidx.compose.ui.unit.TextUnit
  * This composable decreases font size
  * if it can't fit in its parent with given font size.
  *
- * @param scaleDownUntil Stops decreasing font size when the font scale is under or equals to
- * this value even if the device font scale is smaller.
- * @param scaleUpUntil If the composable decreased the font size down to fit in the parent before
- * When there is place again, it increases font size until this scale.
+ * @param autoSizeMinTextSize Stops decreasing font size when the font size is detected under or equal to
+ * this value if this parameter is not equal to TextUnit.Unspecified.
+ * @param autoSizeMaxTextSize If the composable decreased the font size to fit in the parent composable earlier
+ * if there is place again, it increases font size until this value if this value is not TextUnit.Unspecified,
  */
-
 @Composable
 fun AutoSizedText(
     text: String,
     modifier: Modifier = Modifier,
     color: Color = Color.Unspecified,
     fontSize: TextUnit = TextUnit.Unspecified,
-    scaleDownUntil: Double? = null,
-    scaleUpUntil: Double? = null,
+    autoSizeMinTextSize: TextUnit = TextUnit.Unspecified,
+    autoSizeMaxTextSize: TextUnit = TextUnit.Unspecified,
     fontStyle: FontStyle? = null,
     fontWeight: FontWeight? = null,
     fontFamily: FontFamily? = null,
@@ -49,14 +48,7 @@ fun AutoSizedText(
     onTextLayout: (TextLayoutResult) -> Unit = {},
     style: TextStyle = LocalTextStyle.current
 ) {
-    val resizedFontSize = remember {
-        if (fontSize != TextUnit.Unspecified) {
-            mutableStateOf(fontSize)
-        } else {
-            mutableStateOf(style.fontSize)
-        }
-    }
-    val fontScale = LocalDensity.current.fontScale
+    val resizedFontSize = rememberResizedFontSize(fontSize, style)
 
     Text(
         text = text,
@@ -74,15 +66,13 @@ fun AutoSizedText(
         softWrap = softWrap,
         maxLines = maxLines,
         onTextLayout = {
-            if (it.hasVisualOverflow && ((scaleDownUntil != null && scaleDownUntil < resizedFontSize.value.value / fontScale) || scaleDownUntil == null)) {
-                resizedFontSize.value *= 0.9f
-            } else if (
-                (fontSize != TextUnit.Unspecified && fontSize.value < resizedFontSize.value.value)
-                || (fontSize == TextUnit.Unspecified && style.fontSize.value < resizedFontSize.value.value)
-                && ((scaleUpUntil != null && scaleUpUntil > resizedFontSize.value.value / fontScale) || scaleUpUntil == null)
-            ) {
-                resizedFontSize.value *= 1.1f
-            }
+            resizedFontSize.value = getAdjustedFontSize(
+                hasVisualOverflow = it.hasVisualOverflow,
+                currentFontSize = resizedFontSize.value,
+                autoSizeMinTextSize = autoSizeMinTextSize,
+                autoSizeMaxTextSize = autoSizeMaxTextSize,
+                specifiedFontSize = getSpecifiedFontSize(fontSize, style),
+            )
             onTextLayout(it)
         },
         style = style
@@ -95,8 +85,8 @@ fun AutoSizedText(
     modifier: Modifier = Modifier,
     color: Color = Color.Unspecified,
     fontSize: TextUnit = TextUnit.Unspecified,
-    scaleDownUntil: Double? = null,
-    scaleUpUntil: Double? = null,
+    autoSizeMinTextSize: TextUnit = TextUnit.Unspecified,
+    autoSizeMaxTextSize: TextUnit = TextUnit.Unspecified,
     fontStyle: FontStyle? = null,
     fontWeight: FontWeight? = null,
     fontFamily: FontFamily? = null,
@@ -111,14 +101,7 @@ fun AutoSizedText(
     onTextLayout: (TextLayoutResult) -> Unit = {},
     style: TextStyle = LocalTextStyle.current
 ) {
-    val resizedFontSize = remember {
-        if (fontSize != TextUnit.Unspecified) {
-            mutableStateOf(fontSize)
-        } else {
-            mutableStateOf(style.fontSize)
-        }
-    }
-    val fontScale = LocalDensity.current.fontScale
+    val resizedFontSize = rememberResizedFontSize(fontSize, style)
 
     Text(
         text = text,
@@ -137,17 +120,97 @@ fun AutoSizedText(
         maxLines = maxLines,
         inlineContent = inlineContent,
         onTextLayout = {
-            if (it.hasVisualOverflow && ((scaleDownUntil != null && scaleDownUntil < resizedFontSize.value.value / fontScale) || scaleDownUntil == null)) {
-                resizedFontSize.value *= 0.9f
-            } else if (
-                (fontSize != TextUnit.Unspecified && fontSize.value < resizedFontSize.value.value)
-                || (fontSize == TextUnit.Unspecified && style.fontSize.value < resizedFontSize.value.value)
-                && ((scaleUpUntil != null && scaleUpUntil > resizedFontSize.value.value / fontScale) || scaleUpUntil == null)
-            ) {
-                resizedFontSize.value *= 1.1f
-            }
+            resizedFontSize.value = getAdjustedFontSize(
+                hasVisualOverflow = it.hasVisualOverflow,
+                currentFontSize = resizedFontSize.value,
+                autoSizeMinTextSize = autoSizeMinTextSize,
+                autoSizeMaxTextSize = autoSizeMaxTextSize,
+                specifiedFontSize = getSpecifiedFontSize(fontSize, style),
+            )
             onTextLayout(it)
         },
         style = style
     )
+}
+
+@Composable
+private fun rememberResizedFontSize(
+    fontSize: TextUnit,
+    style: TextStyle,
+): MutableState<TextUnit> {
+    return remember {
+        if (fontSize != TextUnit.Unspecified) {
+            mutableStateOf(fontSize)
+        } else {
+            mutableStateOf(style.fontSize)
+        }
+    }
+}
+
+private fun getSpecifiedFontSize(
+    fontSize: TextUnit,
+    style: TextStyle,
+): TextUnit? {
+    return if (fontSize != TextUnit.Unspecified) {
+        fontSize
+    } else if (style.fontSize != TextUnit.Unspecified) {
+        style.fontSize
+    } else null
+}
+
+private fun getAdjustedFontSize(
+    hasVisualOverflow: Boolean,
+    currentFontSize: TextUnit,
+    autoSizeMinTextSize: TextUnit,
+    autoSizeMaxTextSize: TextUnit,
+    specifiedFontSize: TextUnit?,
+): TextUnit {
+    return if (hasVisualOverflow) {
+        getDecreasedFontSize(
+            currentFontSize = currentFontSize,
+            autoSizeMinTextSize = autoSizeMinTextSize
+        )
+    } else {
+        getIncreasedFontSize(
+            currentFontSize = currentFontSize,
+            autoSizeMaxTextSize = autoSizeMaxTextSize,
+            specifiedFontSize = specifiedFontSize,
+        )
+    }
+}
+
+/**
+ * @return If autoSizeMinTextSize is specified and lower than currentFontSize or
+ * if autoSizeMinTextSize is not specified, returns a decreased font size.
+ * Else, returns currentFontSize.
+ */
+private fun getDecreasedFontSize(
+    currentFontSize: TextUnit,
+    autoSizeMinTextSize: TextUnit,
+): TextUnit {
+    return if ((autoSizeMinTextSize != TextUnit.Unspecified && autoSizeMinTextSize < currentFontSize) || autoSizeMinTextSize == TextUnit.Unspecified) {
+        currentFontSize * 0.9f
+    } else currentFontSize
+}
+
+/**
+ * @return If autoSizeMaxTextSize is specified and higher than currentFontSize,
+ * it returns an increased value.
+ *
+ * @return If a font size is specified via fontSize or style parameters
+ * and current font size is smaller than specified font size, it returns
+ * an increased value. Else, it returns specifiedFontSize.
+ */
+private fun getIncreasedFontSize(
+    currentFontSize: TextUnit,
+    autoSizeMaxTextSize: TextUnit,
+    specifiedFontSize: TextUnit?,
+): TextUnit {
+    if (specifiedFontSize != null && currentFontSize > specifiedFontSize) return specifiedFontSize
+
+    if ((autoSizeMaxTextSize != TextUnit.Unspecified && autoSizeMaxTextSize > currentFontSize) || autoSizeMaxTextSize == TextUnit.Unspecified) {
+        currentFontSize * 1.1f
+    }
+
+    return currentFontSize
 }
